28de4fdb7c947688c34d5f06e9e8654e8b47c4f0
vIkAsSiNgH <76863611+vikkyak@users.noreply.github.com>
2024-05-13 15:34:33 +0900

    Update T2MF.m

--- T2MF.m
+++ T2MF.m
@@ -8,13 +8,18 @@ mu=zeros(1,H);
 for q=1:H;
     mu(1,q)=KMM(q,vector);
 end
+% average_mu=((sum(mu))/H)*ones(1,p);
+% lembda= 4.5*abs(vector-average_mu);
+% mu_Mat=repmat(mu,p,1)';
+% lembda_vector=repmat(vector,H,1);
+% sigma=KMM(H,lembda);
 average_mu=((sum(mu))/H)*ones(1,p);
-lembda= 4.5*abs(vector-average_mu);
+lembda= abs(vector-average_mu);
 mu_Mat=repmat(mu,p,1)';
 lembda_vector=repmat(vector,H,1);
-sigma=KMM(H,lembda);
-if sigma < 0.0001
-    sigma=0.0001;
+[~,~,sigma]=ksdensity(lembda);
+if sigma < 0.01
+    sigma=0.01;
 end
 PI= exp(-0.5*((lembda_vector-mu_Mat)/sigma).^2);
 T_max=max(max(PI));
